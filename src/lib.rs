c2a90672269c5c59ac1588cbc1f8ba904507dc15
iubns <iubns@naver.com>
2023-09-20 19:20:52 +0900

    add: add fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use wasm_bindgen::prelude::*;
+use std::collections::VecDeque;
 
 #[wasm_bindgen]
 extern "C" {
@@ -12,44 +13,58 @@ pub fn greet(content: &str){
 }
 
 enum CommandType {
-    Insert(i8),
-    Out(i32),
+    Push,
+    Pop,
     Add,
     Sub,
     Mul,
     Div,
     Mod,
     None,
-    Pop,
+    Select,
+    Move,
+    Equal,
+    Condition,
+    Change,
+    Duple,
     Exit,
 }
 struct Command {
     way: (i8, i8),
-    command_type: CommandType
+    command_type: CommandType,
+    next_storage: StorageType,
 }
 
 impl Command {
-    fn new(command_type: CommandType, way: (i8, i8)) -> Command{
+    fn new(command_type: CommandType, way: (i8, i8), next_storage: StorageType) -> Command{
         Command{
             way,
             command_type,
+            next_storage,
         }
     }
 }
 
+
 fn get_command_type(first_char: &u32) -> CommandType {
     println!("first_char {}", &first_char);
     match first_char {
-        0 | 1 => CommandType::None, //ㄱㄲ
+        0 | 1 | 15 => CommandType::None, //ㄱ ㄲ ㅋ
         3 => CommandType::Add, //ㄷ
         16 => CommandType::Sub, //ㅌ
         4 => CommandType::Mul, // ㄸ
         2 => CommandType::Div, // ㄴ
         5 => CommandType::Mod, // ㄹ
         11 => CommandType::None, //ㅇ
-        7 => CommandType::Insert(0), //ㅂ
+        7 => CommandType::Push, //ㅂ
         6 => CommandType::Pop, //ㅁ
-
+        9 => CommandType::Select, // ㅅ
+        12 => CommandType::Equal, // ㅈ
+        14 => CommandType::Condition, // ㅊ
+        17 => CommandType::Change, // ㅍ
+        18 => CommandType::Exit, // ㅎ
+        10 => CommandType::Move, // ㅆ
+        8 => CommandType::Duple, // ㅃ
         _ => CommandType::Exit,
     }
 }
@@ -66,6 +81,7 @@ fn get_move_way(second_char: &u32) -> (i8, i8){
         13 => (0, 1),// ㅜ
         17 => (0, 2),// ㅠ
 
+        //Todo: 모음 이동 방향 더 해야 함
         18 => (0, 0),// ㅡ
         19 => (0, 0), // ㅢ
         20 => (0, 0),// ㅣ
@@ -73,10 +89,27 @@ fn get_move_way(second_char: &u32) -> (i8, i8){
     }
 }
 
+enum StorageType {
+    Stack(usize),
+    Queue,
+}
 
-pub fn run(content: &str) -> &str
-{
-    let mut content_array = Vec::new();
+fn get_storage_type (third_char: u32) -> StorageType{
+    match third_char {
+        11 => StorageType::Queue,
+        _ => StorageType::Stack(third_char as usize),
+    }
+}
+
+fn init_storage (storage: &mut Vec<Vec<i32>>){
+    for _ in 0..26  {
+        storage.push(Vec::new())
+    }
+}
+
+
+fn parse(content: & str) -> Vec<Vec<char>>{
+ let mut content_array = Vec::new();
     let lines = content.split('\n');
     for line in lines {
         let mut line_array = Vec::new();
@@ -85,10 +118,20 @@ pub fn run(content: &str) -> &str
             line_array.push(c);
         };
         content_array.push(line_array);
-    }
+    };
+    content_array
+}
 
+pub fn run(content: &str) -> &str
+{
+    let mut _storage : Vec<Vec<i32>> = Vec::new();
+    let mut _queue: VecDeque<i32> = VecDeque::new();
+    init_storage(&mut _storage);
+
+    let content_array = parse(&content);
 
     let mut _position = (0, 0);
+    let mut _current_storage_type = StorageType::Stack(0);
     loop {
         let line_array = content_array.get(_position.1);
 
@@ -111,12 +154,18 @@ pub fn run(content: &str) -> &str
         let x = _position.0 as i8 + cmd.way.0;
         let y = _position.1 as i8 + cmd.way.1;
 
-        match cmd.command_type {
-            CommandType::Exit => {
+        match (&cmd.command_type, &_current_storage_type) {
+            (CommandType::Exit, _) => {
                 println!("done!");
                 break
             },
-            _ => run_command(cmd),
+            (_, StorageType::Stack(stack_mun)) => {
+                let stack_option = _storage.get_mut(*stack_mun);
+                if let Some(stack) = stack_option  {
+                    run_command(cmd, &_current_storage_type, stack, &mut _queue)
+                }
+            },
+            _ => ()
         }
         
 
@@ -136,13 +185,38 @@ fn get_command(char: &char) -> Command {
     
     let command_type = get_command_type(&first_char);
     let way = get_move_way(&second_char);
+    let next_storage_type = get_storage_type(third_char);
 
-    return  Command::new(command_type, way);
+    return  Command::new(command_type, way, next_storage_type);
 }
 
 
-fn run_command(cmd:Command){
-    match cmd {
+fn run_command(cmd:Command, current_storage_type: &StorageType, storage: &mut Vec<i32>, queue: &mut VecDeque<i32>){
+    match cmd.command_type {
+        CommandType::Add => add(current_storage_type, storage, queue),
         _ => println!("a")
+    }
+}
+
+
+fn add(storage_type: &StorageType, stack: &mut Vec<i32>, queue: &mut VecDeque<i32>) {
+    if let storage_type = StorageType::Queue {
+        let first = queue.pop_back();
+        let second = queue.pop_back();
+
+        match (first,second) {
+            (Some(x), Some(y)) => queue.push_back( x + y),
+            _ => (),
+        }
+    }
+
+    if let StorageType::Stack(_stack_num) = *storage_type {
+        let first = stack.pop();
+        let second = stack.pop();
+
+        match (first, second) {
+            (Some(x), Some(y)) => stack.push(x + y),
+            _ => (),
+        }
     }
 }
